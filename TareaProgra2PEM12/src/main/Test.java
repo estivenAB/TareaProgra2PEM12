649225057e9a7257f23263c66eadaa8b90113a08
Estiven <Estiven@DESKTOP-ENN0B09>
2018-12-05 21:34:52 -0600

    Agregación de clases

--- TareaProgra2PEM12/src/main/Test.java
+++ TareaProgra2PEM12/src/main/Test.java
@@ -1,6 +1,6 @@
 package main;
 
-import Interaction.Welcome;
+import view.Welcome;
 
 /**
  *
--- TareaProgra2PEM12/src/view/Login.java
+++ TareaProgra2PEM12/src/view/Login.java
@@ -1,4 +1,4 @@
-package Interaction;
+package view;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
